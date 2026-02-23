255bef6d70003183d51e097d6d9fd76ebfbd79fa
Stefan Eenjes <stefaneenjes96@gmail.com>
2026-02-22 20:58:42 -0900

    Make Checkwin function work properly

--- script.js
+++ script.js
@@ -81,17 +81,73 @@ function GameController(
             console.log("Please choose a different cell.")
         }
 
+        checkForWin();
         switchPlayerTurn();
         printNewRound();
     }
 
     const checkForWin = () => {
-        console.log(board.getBoard())
+        let colums = [
+            {
+                x: 0,
+                o: 0
+            },
+            {
+                x: 0,
+                o: 0
+            },
+            {
+                x: 0,
+                o: 0
+            }
+        ];
+
+        const verticalWinCondition = (token) => {
+            return colums[0][token] === 3 | colums[1][token] === 3 | colums[2][token] === 3;
+        }
+
+        const diagonalWinCondition = (token) => {
+            const gameboard = board.getBoard();
+            console.log(`${gameboard[0][0].getValue()} and ${gameboard[1][1].getValue()} and ${gameboard[2][2].getValue()}`);
+            return (gameboard[0][0].getValue() === token && gameboard[1][1].getValue() === token && gameboard[2][2].getValue() === token) | 
+                (gameboard[0][2].getValue() === token && gameboard[1][1].getValue() === token && gameboard[2][0].getValue() === token)
+            
+        }
+
+        const horizontalWinCondition = (token) => {
+            let win = false
+            board.getBoard().forEach(row => {
+
+                let counter = 0;
+                for (let i = 0; i < row.length; i++) {
+                    if (row[i].getValue() === token) {
+                        colums[i].o++;
+                        counter++;
+                    } 
+                }  
+
+                if (counter === 3) {
+                    win = true;
+                }           
+            });
+
+            return win;
     }
+
+        console.log(diagonalWinCondition("O"))
+
+        if (horizontalWinCondition("O") | verticalWinCondition("o") | diagonalWinCondition("O")) {
+            console.log("Player One has won!")
+        } else if (horizontalWinCondition("X") | verticalWinCondition("x") | diagonalWinCondition("X")) {
+            console.log("Player Two has won!")
+        } else {
+            console.log("no winner yet.")
+        };
+    };
 
     return {
         playRound,
-        checkForWin
+        checkForWin,
     }
 }
 
